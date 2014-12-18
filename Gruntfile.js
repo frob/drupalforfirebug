a9c190029664f4f2a537d8be3b4be996857e6bc8
frob <frob@249517.no-reply.drupal.org>
2014-12-17 23:30:03 -0800

    made extension creation with grunt mozilla addon sdk

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,6 +10,12 @@ module.exports = function(grunt) {
             cwd: 'src/',
             src: ['**'],
             dest: 'build'
+          },
+          {
+            expand: true,
+            cwd: 'templates/firefox/',
+            src: ['package.json'],
+            dest: 'build'
           }
         ]
       }
@@ -30,16 +36,84 @@ module.exports = function(grunt) {
         dest: ''
       }
     },
-    watch: {
-    scripts: {
-      files: ['src/*.js'],
-      tasks: ['clean', 'copy'],
-      options: {
-        spawn: false,
+    sed: {
+      name: {
+        path: 'build/',
+        pattern: '%PKG.NAME%',
+        replacement: '<%= pkg.name %>',
+        recursive: true
       },
+      title: {
+        path: 'build/',
+        pattern: '%PKG.TITLE%',
+        replacement: '<%= pkg.title || pkg.name %>',
+        recursive: true
+      },
+      description: {
+        path: 'build/',
+        pattern: '%PKG.DESCRIPTION%',
+        replacement: '<%= pkg.description %>',
+        recursive: true
+      },
+      homepage: {
+        path: 'build/',
+        pattern: '%PKG.HOMEPAGE%',
+        replacement: '<%= pkg.homepage || "" %>',
+        recursive: true
+      },
+      author: {
+        path: 'build/',
+        pattern: '%PKG.AUTHOR%',
+        replacement: '<%= pkg.author.name %>',
+        recursive: true
+      },
+      icon: {
+        path: 'build/',
+        pattern: '%PKG.ICON%',
+        replacement: '<%= pkg.icon || "icon.png" %>',
+        recursive: true
+      },
+      license: {
+        path: 'build/',
+        pattern: '%PKG.LICENSE%',
+        replacement: '<%= _.pluck(pkg.licenses, "type").join(", ") %>',
+        recursive: true
+      },
+      version: {
+        path: 'build/',
+        pattern: '%PKG.VERSION%',
+        replacement: '<%= pkg.version %>',
+        recursive: true
+      }
+    },
+    watch: {
+      scripts: {
+        files: ['src/*.js'],
+        tasks: ['clean', 'copy'],
+        options: {
+          spawn: false,
+        },
+      },
+    },
+    "mozilla-addon-sdk" : {
+      'release': {
+        options: {
+          revision: "1.16"
+        }
+      }
+    },
+    "mozilla-cfx-xpi" : {
+      'release': {
+        options: {
+          "mozilla-addon-sdk": "release",
+          extension_dir: "build",
+          dist_dir: "release/build",
+          // --output-file is an experimental option, not guaranteed to exist.
+          // @see https://developer.mozilla.org/en-US/Add-ons/SDK/Tools/cfx#Experimental_Options_3
+          arguments: "--output-file=<%= pkg.name %>.xpi"
+        }
+      }
     }
-}
-
   });
 
   grunt.loadNpmTasks('grunt-contrib-concat');
@@ -47,8 +121,11 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-compress');
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-sed');
+  grunt.loadNpmTasks('grunt-mozilla-addon-sdk');
 
 
-  grunt.registerTask('default', ['clean', 'copy']);
-  grunt.registerTask('release', ['clean', 'copy', 'compress']);
+  grunt.registerTask('default', ['clean', 'copy', 'sed']);
+  grunt.registerTask('release', ['clean', 'copy', 'sed', 'compress']);
+  grunt.registerTask('build', 'Builds the Firefox extension.', ['mozilla-cfx-xpi']);
 };
